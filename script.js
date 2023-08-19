43ea2969e64bdd196fd85412f5a865baceca0c32
jasondipopolo <jasondipopolo@gmail.com>
2023-08-19 07:36:29 -0400

    Applies color w/o clicking btn, added exportOption

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Ensures JS only loads *after* HTML
 document.addEventListener('DOMContentLoaded', () => {
+
+// Referencing Elements in the DOM
     const svgFileInput = document.getElementById('svgFileInput');
     const colorPicker = document.getElementById('colorPicker');
     const applyColorButton = document.getElementById('applyColorButton');
     const canvas = document.getElementById('canvas');
-  
+
+// Event Listener for File Input
     svgFileInput.addEventListener('change', handleFileInput);
   
     function handleFileInput(event) {
@@ -18,14 +22,20 @@ document.addEventListener('DOMContentLoaded', () => {
         reader.readAsText(file);
       }
     }
-  
+
+// Listens for color change
     function addColorChangeListeners() {
       const shapes = canvas.querySelectorAll('path, circle, rect'); // Add other SVG shapes as needed
       shapes.forEach((shape) => {
         shape.addEventListener('click', handleShapeClick);
+        //If fill is none, apply white because otherwise, shape is unclickable
+        if (shape.style.fill = 'none') {
+            shape.style.fill = 'white';
+          }          
       });
     }
   
+// Handles shape click
     function handleShapeClick(event) {
       const selectedColor = colorPicker.value;
       const shape = event.target;
@@ -40,7 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
         shape.setAttribute('fill', selectedColor);
       }
     }
-  
+
+// Apply color button listener
     applyColorButton.addEventListener('click', () => {
       const selectedColor = colorPicker.value;
       const shapes = canvas.querySelectorAll('path, circle, rect'); // Add other SVG shapes as needed
@@ -53,5 +64,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     });
+
+
+// Export SVG Button
+const exportSvgButton = document.getElementById('exportSvgButton');
+exportSvgButton.addEventListener('click', exportSvg);
+
+function exportSvg() {
+    const svgContent = canvas.innerHTML;
+    const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'modified_svg.svg';
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
   });
   
