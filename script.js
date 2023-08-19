1e733e1ab2d65a7717c331d7075bee9cefeac383
jasondipopolo <jasondipopolo@gmail.com>
2023-08-19 14:52:08 -0400

    Major, styling, user can select preset grids

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const colorPicker = document.getElementById('colorPicker');
     const applyColorButton = document.getElementById('applyColorButton');
     const canvas = document.getElementById('canvas');
+    const app = document.getElementById('app');
 
+    function showSvgEditTools(){
+        colorPicker.style.visibility = 'visible'
+        applyColorButton.style.visibility = 'visible'
+        exportSvgButton.style.visibility = 'visible'
+    }
+    
 // Event Listener for File Input
     svgFileInput.addEventListener('change', handleFileInput);
   
@@ -17,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         reader.onload = (e) => {
           const svgContent = e.target.result;
           canvas.innerHTML = svgContent;
+          showSvgEditTools();
           addColorChangeListeners();
         };
         reader.readAsText(file);
@@ -28,8 +36,9 @@ document.addEventListener('DOMContentLoaded', () => {
       const shapes = canvas.querySelectorAll('path, circle, rect'); // Add other SVG shapes as needed
       shapes.forEach((shape) => {
         shape.addEventListener('click', handleShapeClick);
+
         //If fill is none, apply white because otherwise, shape is unclickable
-        if (shape.style.fill = 'none') {
+        if (shape.style.fill = 'none' || shape.style.fill === 'none' ) {
             shape.style.fill = 'white';
           }          
       });
@@ -84,6 +93,59 @@ function exportSvg() {
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
 }
+
+
+
+// Grid Selector Logic
+const gridSelector = document.getElementById('grid-selector');
+    
+    gridSelector.addEventListener('change', handleGridSelection);
+    
+    function handleGridSelection() {
+        const selectedOption = gridSelector.value;
+        const svgUrl = getSvgUrlForOption(selectedOption);
+        
+        fetchAndDisplaySvg(svgUrl);
+    }
+    
+    function getSvgUrlForOption(selectedOption) {
+        // Define a mapping of options to corresponding SVG URLs
+        const svgUrlMap = {
+            'square-radial': 'https://gistcdn.githack.com/jasondipopolo/45a2247976364937cf3e21cc2528abf9/raw/46727c0f54a16f3e16353d4594eccd0f69231fa9/square-radial.svg',
+            'square': 'https://gistcdn.githack.com/jasondipopolo/38b6aab5cd34ed555e6d3816c76ba5a0/raw/1d7cc313bb8b26b29b689446541584d3d757631e/square.svg',
+            'radial': 'https://gistcdn.githack.com/jasondipopolo/2b1b0ae0acb89ed7259230fefe095e0f/raw/cfa3598a4f27fb90a3af036f95247f1bbeb97e36/radial.svg',
+            // Add other options and URLs here
+        };
+        
+        return svgUrlMap[selectedOption];
+    }
+    
+    function fetchAndDisplaySvg(svgUrl) {
+        fetch(svgUrl)
+            .then(response => response.text())
+            .then(svg => {
+                canvas.innerHTML = svg;
+                showSvgEditTools()
+                addColorChangeListeners();
+            });
+    }
+
+    // Modify the exportSvg function to handle both user-uploaded SVGs and fetched SVGs
+    function exportSvg() {
+        const svgContent = canvas.innerHTML;
+        const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'modified_svg.svg';
+        document.body.appendChild(a);
+        a.click();
+
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+}
+
 
   });
   
